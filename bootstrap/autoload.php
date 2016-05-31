89b550111a64193375a76848a068883d55d2b761
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-05-31 22:18:55 +0200

    Add bootstrap directory

---
+++ bootstrap/autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Register the Composer auto loader.
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Load the application configuration.
+ */
+require_once __DIR__ . '/../config/application.php';
--- web/index.php
+++ web/index.php
@@ -1,5 +1,13 @@
 <?php
 
-/** WordPress view bootstrapper */
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
 define( 'WP_USE_THEMES', true );
+
+/**
+ * Load WordPress.
+ */
 require __DIR__ . '/wp/wp-blog-header.php';
