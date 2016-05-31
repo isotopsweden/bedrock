f540fc56296c76d8d7a65a658da212b4261f2a1d
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-05-31 15:30:33 +0200

    Add echos to before apache script

--- docker/scripts/before-apache.sh
+++ docker/scripts/before-apache.sh
@@ -5,7 +5,9 @@
 echo "Running before apache script"
 
 cd /var/www/html
+
+echo "Running composer install"
 composer install
 
-cd /var/www/html/web
+echo "Running search replace with wp-cli"
 wp --allow-root --all-tables search-replace $WP_HOME $VIRTUAL_HOST
